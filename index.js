f69bca11e19fef1a1b75387e091e5a5d96fa9a51
Manya Verma <manyapawanverma@gmail.com>
2022-08-24 14:54:00 +0530

    added stripe

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute=require("./routes/product");
+const cartRoute=require("./routes/cart");
+const orderRoute=require("./routes/order");
 
 dotenv.config();
 
@@ -17,7 +19,9 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
-app.use("/api/product",productRoute);
+app.use("/api/products",productRoute);
+app.use("/api/carts",cartRoute);
+app.use("/api/orders",orderRoute);
 
 
 
